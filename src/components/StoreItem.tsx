c2ca3b4a7cb8ccf08f56356bc539ae0e2ec5d9c1
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-10 12:48:00 +0100

    Display item name and price in store item component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -10,6 +10,12 @@ type StoreItemProps = {
 export function StoreItem ({id, name, price, imgUrl}:StoreItemProps){
 return <Card>
     <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit: 'cover'}} />
+    <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+         <span className="fs-2">{name}</span>
+         <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+    </Card.Body>
 </Card>
 
 }
