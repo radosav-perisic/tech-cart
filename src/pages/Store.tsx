5b562000525607bf4a3b80fbddfe98414cc10a65
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-10 12:19:56 +0100

    Add StoreItem component and display items as cards
    This commit adds the 'StoreItem' component to display store items as cards. The component takes in item properties and displays the item's image using 'Card' from 'react-bootstrap'. The 'Store' file has also been updated to map over the items and render 'StoreItem' components.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,14 +1,17 @@
 import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 
 export const Store = () => {
   return (
     <>
       <h1>Store</h1>
-      <Row>
-         {storeItems.map(item => (
-            <Col>{JSON.stringify(item)}</Col>
-         ))}
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
       </Row>
     </>
   );
