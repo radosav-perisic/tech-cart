278a2f8a38f4553def09c4bff01261f2011027cc
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-10 11:53:38 +0100

    Feature: Display store items using react-bootstrap components
    Add item data to store page and display using react-bootstrap components

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+
 export const Store = () => {
-   return  <h1>Store</h1>
-}
+  return (
+    <>
+      <h1>Store</h1>
+      <Row>
+         {storeItems.map(item => (
+            <Col>{JSON.stringify(item)}</Col>
+         ))}
+      </Row>
+    </>
+  );
+};
