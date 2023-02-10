fbe90a447ed5f9202403c02a95868dd98add4580
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-10 13:26:41 +0100

    Use currency formatter for store item price display

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
     id: number,
@@ -11,9 +12,9 @@ export function StoreItem ({id, name, price, imgUrl}:StoreItemProps){
 return <Card>
     <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit: 'cover'}} />
     <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
          <span className="fs-2">{name}</span>
-         <span className="ms-2 text-muted">{price}</span>
+         <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
     </Card.Body>
 </Card>
