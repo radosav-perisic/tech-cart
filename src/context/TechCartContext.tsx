bb40a8acd99f97744c3dc5a554be63590d014e80
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-11 17:53:07 +0100

    Implement Shopping Cart Context Functions

--- src/context/TechCartContext.tsx
+++ src/context/TechCartContext.tsx
@@ -1,17 +1,75 @@
-import { createContext, useContext, ReactNode } from "react";
+import { createContext, useContext, ReactNode, useState } from "react";
+import { isHtmlElement } from "react-router-dom/dist/dom";
 
 type TechCartProviderProps = {
   children: ReactNode;
 };
 
-const TechCartContext = createContext({});
+type CartItem = {
+    id: number 
+    quantity: number
+}
+
+type ShoppingCartContext = {
+    getItemQuantity: (id:number) => number
+    increaseCartQuantity: (id:number) =>void
+    decreaseCartQuantity: (id:number) =>void
+    removeFromCart: (id:number) => void
+}
+
+
+const TechCartContext = createContext({} as ShoppingCartContext);
 
 export function useTechCart() {
   return useContext(TechCartContext);
 }
 
 export function TechCartProvider({ children }: TechCartProviderProps) {
+     const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+     function getItemQuantity(id:number) {
+        return cartItems.find(item=> item.id === id)?.quantity || 0
+     }  
+
+     function increaseCartQuantity(id:number) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null){
+                return [...currItems, {id, quantity: 1}]
+            } else {
+                return currItems.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity + 1}
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+     }
+     function decreaseCartQuantity(id:number) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1){
+                return currItems.filter(item => item.id !== id)
+            } else {
+                return currItems.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity - 1}
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+     }
+
+    function removeFromCart(id:number) {
+      setCartItems(currItems => {
+        return currItems.filter(item => item.id !== id)
+      }) 
+    }
+
+
   return (
-    <TechCartContext.Provider value={{}}>{children}</TechCartContext.Provider>
+    <TechCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>{children}</TechCartContext.Provider>
   );
 }
