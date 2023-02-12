b71444debf0eb63b970f1f3e496e1e5433a7a766
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-12 12:42:51 +0100

    Enhance TechCart with cart state management

--- src/context/TechCartContext.tsx
+++ src/context/TechCartContext.tsx
@@ -6,17 +6,20 @@ type TechCartProviderProps = {
 };
 
 type CartItem = {
-    id: number 
-    quantity: number
-}
+  id: number;
+  quantity: number;
+};
 
 type ShoppingCartContext = {
-    getItemQuantity: (id:number) => number
-    increaseCartQuantity: (id:number) =>void
-    decreaseCartQuantity: (id:number) =>void
-    removeFromCart: (id:number) => void
-}
-
+  openCart: () => void;
+  closeCart: () => void;
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+};
 
 const TechCartContext = createContext({} as ShoppingCartContext);
 
@@ -25,51 +28,69 @@ export function useTechCart() {
 }
 
 export function TechCartProvider({ children }: TechCartProviderProps) {
-     const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [isOpen, setIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-     function getItemQuantity(id:number) {
-        return cartItems.find(item=> item.id === id)?.quantity || 0
-     }  
+  const cartQuantity = cartItems.reduce((quantity,item) => item.quantity + quantity, 0)
 
-     function increaseCartQuantity(id:number) {
-        setCartItems(currItems => {
-            if (currItems.find(item => item.id === id) == null){
-                return [...currItems, {id, quantity: 1}]
-            } else {
-                return currItems.map(item => {
-                    if(item.id === id) {
-                        return {...item, quantity: item.quantity + 1}
-                    } else {
-                        return item
-                    }
-                })
-            }
-        })
-     }
-     function decreaseCartQuantity(id:number) {
-        setCartItems(currItems => {
-            if (currItems.find(item => item.id === id)?.quantity === 1){
-                return currItems.filter(item => item.id !== id)
-            } else {
-                return currItems.map(item => {
-                    if(item.id === id) {
-                        return {...item, quantity: item.quantity - 1}
-                    } else {
-                        return item
-                    }
-                })
-            }
-        })
-     }
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(true);
 
-    function removeFromCart(id:number) {
-      setCartItems(currItems => {
-        return currItems.filter(item => item.id !== id)
-      }) 
-    }
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
 
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  }
 
   return (
-    <TechCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>{children}</TechCartContext.Provider>
+    <TechCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
+      }}
+    >
+      {children}
+    </TechCartContext.Provider>
   );
 }
