c9ce2f76ab759e541ae93d9719dcbf2c87a948ad
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-13 10:14:37 +0100

    Tech Cart Total Price Implementation

--- src/components/TechCart.tsx
+++ src/components/TechCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useTechCart } from "../context/TechCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json";
 
 type shoppingCartProps = {
   isOpen: boolean;
@@ -18,6 +20,12 @@ export function TechCart({ isOpen }: shoppingCartProps) {
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total {""}{formatCurrency(cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find((i) => i.id === cartItem.id)
+              return total + (item?.price || 0 ) * cartItem.quantity
+            }, 0))}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
