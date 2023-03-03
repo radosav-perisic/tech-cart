057acc287cbbc7fc718132a7200a9d6995c48f72
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-03 20:19:37 +0100

    Modify About page to include detailed description

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,10 @@
 export const About = () => {
-   return  <h1>About</h1>
+   return (
+      <div className="d-flex flex-column align-items-center my-5">
+  <h1 className="text-center font-weight-bold mb-4">About The Gadget Cart</h1>
+  <p className="text-justify w-75 mb-4">The Gadget Cart is a one-stop shop for all your technology needs. From the latest smartphones to laptops, we have it all. We strive to provide our customers with the best products and services, and our team of experts is always on hand to help with any questions or concerns.</p>
+  <p className="text-justify w-75 mb-4">At The Gadget Cart, we believe in the power of technology to make people's lives easier and more connected. That's why we only stock the latest and greatest products, and why we offer such competitive prices. So whether you're in the market for a new device or just want to stay up-to-date with the latest tech, you'll find everything you need at The Gadget Cart.</p>
+  <p className="text-center font-weight-bold mb-0">Visit us today and see what we have in store for you!</p>
+</div>
+    );
 }
